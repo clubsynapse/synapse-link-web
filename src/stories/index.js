ed26d0374068b9c0442f58fff4d64d011a84a722
Papa Seynou Sy Ndiaye <seynousy@gmail.com>
2018-03-13 22:02:27 +0000

    feat: added post cards component on storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import { Button, Welcome } from '@storybook/react/demo';
-import { Flag } from "semantic-ui-react"
+import { Button, Welcome } from "@storybook/react/demo";
+import Post from "../components/Post";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+  ));
 
-storiesOf('Semantic Ui React', module).add('Flag Senegal', () => <Flag name="sn" />);
+storiesOf("Posts", module)
+  .add("default post (just text)", () => <Post />)
+  .add("image post", () => <Post type="image" />)
+  .add("video post", () => <Post type="video" />);
